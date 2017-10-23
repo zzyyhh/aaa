40f791fe2d96f0e6f637eb08725be98ccfbbe5ef
yinghui.zhang <zyh.zyh@alibaba-inc.com>
2017-10-23 20:52:11 +0800

    info

---
+++ src/main/java/gof4test/Test.java
@@ -0,0 +1,7 @@
+package gof4test;
+
+/**
+ * @author yinghui.zhang on 2017/10/23
+ */
+public class Test {
+}
---
+++ src/main/java/gof4test/visitor/CommonVisitor.java
@@ -0,0 +1,64 @@
+package gof4test.visitor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+/**
+ * @author yinghui.zhang on 2017/10/23
+ */
+public class CommonVisitor {
+    interface Visitor<T> {
+        T visit(Square element);
+        T visit(Circle element);
+    }
+    interface Element {
+        <T> T accept(Visitor<T> visitor);
+    }
+
+    public static class Square implements Element {
+        private double side;
+
+        public Square(double side) {
+            this.side = side;
+        }
+
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visit(this);
+        }
+    }
+
+    public static class Circle implements Element {
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visit(this);
+        }
+    }
+
+    public static class AreaVisitor implements Visitor<Double> {
+
+        public Double visit(Square element) {
+            return element.side * element.side;
+        }
+
+        public Double visit(Circle element) {
+            return Math.PI * element.radius * element.radius;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Element> all = Lists.newArrayList(new Circle(4), new Square(2));
+        Visitor<Double> visitor = new AreaVisitor();
+        double total = 0.0;
+        for (Element element : all) {
+            total += element.accept(visitor);
+        }
+        System.out.println(total);
+    }
+}
